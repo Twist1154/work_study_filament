7d0b48e72dd686e1e1b2aece3bd4e92b621673e3
Twist1154 <rethabile.dev@gmail.com>
2026-06-29 12:19:13 +0200

    Add `StudentDashboard` Filament page with custom title

---
+++ app/Filament/Student/Pages/StudentDashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Student\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class StudentDashboard extends BaseDashboard
+{
+    protected static ?string $title = 'Student Dashboard';
+}
